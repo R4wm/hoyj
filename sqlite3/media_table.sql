d356005e0e67a5a6c1e9dbd0f62f191999f625ca
raymondmintz11@gmail.com <raymondmintz11@gmail.com>
2024-04-22 18:01:24 -0400

    create database

---
+++ sqlite3/media_table.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS media(
+file_path TEXT PRIMARY KEY,
+speaker_fn TEXT,
+speaker_ln TEXT,
+series_title TEXT,
+book_name TEXT,
+chapter INTEGER,
+media_title TEXT,
+url_path TEXT,
+file_type TEXT,
+media_type TEXT
+)
