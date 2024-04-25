8d0eb3ccdba064e1f864b4a0bf65457faa563125
r4wm <raymondmintz11@gmail.com>
2024-04-25 01:41:11 +0000

    finish script to dump object storage

--- move_off_cloud/download_all_from_cloud.sh
+++ move_off_cloud/download_all_from_cloud.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
+set -e
 
+linode-cli obj ls  | awk '{print $3}'
+
+MEDIADIR='/mnt/new_storage/media'
+
+cd /mnt/new_storage/media
+
+for i in $(linode-cli obj ls  | awk '{print $3}' )
+do
+    for j in  $(linode-cli obj ls $i | awk '{print $4}')
+    do
+        echo "linode-cli obj get $i $j $MEDIADIR/$j"
+        if ! [ -f "$MEDIADIR/$j" ]
+        then
+            linode-cli obj get "$i" "$j" "$MEDIADIR/$j"
+        else
+            echo ">>> $MEDIADIR/$j already exists, skipping..."
+        fi
+    done
+done
